a2b84a24ad815b8072e9756cfbc3f47968a6abb5
Iulian Dumitrache <iulian_alexandru04@yahoo.com>
2025-02-09 17:51:54 +0200

    add script for most obscure superhero

--- superhero-popularity/most-obscure-superhero.py
+++ superhero-popularity/most-obscure-superhero.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
-spark = SparkSession.builder.appName("MostPopularSuperhero").getOrCreate()
+spark = SparkSession.builder.appName("MostObscureSuperhero").getOrCreate()
 
 schema = StructType([ \
                      StructField("id", IntegerType(), True), \
@@ -18,11 +18,15 @@ connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " 
     .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " ")) - 1) \
     .groupBy("id").agg(func.sum("connections").alias("connections"))
     
-mostPopular = connections.sort(func.col("connections").desc()).first()
+minConnectionCount = connections.agg(func.min('connections')).first()[0]
 
-mostPopularName = names.filter(func.col("id") == mostPopular[0]).select("name").first()
+minConnections = connections.filter(func.col('connections') == minConnectionCount)
 
-print(mostPopularName[0] + " is the most popular superhero with " + str(mostPopular[1]) + " co-appearances.")
+minConnectionsWithNames = minConnections.join(names, 'id')
+
+print(f'The following characters have only {minConnectionCount} connection(s):')
+
+minConnectionsWithNames.select('name').show()
 
 spark.stop()
 
